2f45e21d6d5522572880dfb0b820efeffbc3f866
H_RAF <rafhbegin@gmail.com>
2018-10-29 11:36:17 +0100

    V2

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: maberkan <maberkan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/24 08:49:10 by maberkan     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/25 15:13:33 by maberkan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/29 11:20:54 by maberkan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "get_next_line.h"
 
 char	*read_line(char *str, int fd)
@@ -31,21 +32,25 @@ int		get_next_line(const int fd, char **line)
 	int			i;
 	static char	*str;
 
-	str = read_line(str, fd);
-	if (fd < 1 || line == NULL)
-		return (-1);
-	if (!(*line = ft_strnew(0)))
-		return (0);
-	i = 0;
-	while (str && str[i] != '\n' && ++i)
+	if (!str)
 	{
-		if (str[i] == '\n')
+		str = ft_strnew(0);
+		str = read_line(str, fd);
+	}
+	if (fd < 0 || line == NULL || read(fd, *line, 0) < 0)
+		return (-1);
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] != '\n' && str[i] != '\0')
+			i++;
+		if (str[i] == '\n' || str[i] == '\0')
 		{
 			*line = ft_strsub(str, 0, i);
 			str = ft_strsub(str, i + 1, ft_strlen(str) - i);
 			return (1);
 		}
-		*line = ft_strsub(str, 0, ft_strlen(str) - i);
 	}
+	*line = NULL;
 	return (0);
 }
